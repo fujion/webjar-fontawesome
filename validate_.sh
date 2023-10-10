0c2ea61bca4badf405564d173d258bf88d5f6c8f
Douglas Martin <mdgeek1@gmail.com>
2023-10-10 17:23:05 -0400

    Use new webjar validator plugin.

--- validate_.sh
+++
@@ -1,7 +0,0 @@
-file_exists "import-map.json"
-file_contains "README.md" "SIL OFL"
-file_contains "README.md" "MIT License"
-file_contains "README.md" "CC BY 3.0 License"
-
-file_exists "dist/css/font-awesome.css"
-file_exists "dist/fonts/FontAwesome.otf"
